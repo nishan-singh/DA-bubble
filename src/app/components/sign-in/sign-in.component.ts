1390c962f17e76855847220755a2f87a214dd1e0
Nishan Singh <singh007nishan@gmail.com>
2023-08-21 17:52:02 +0200

    added animation

--- src/app/components/sign-in/sign-in.component.ts
+++ src/app/components/sign-in/sign-in.component.ts
@@ -3,8 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-sign-in',
   templateUrl: './sign-in.component.html',
-  styleUrls: ['./sign-in.component.scss']
+  styleUrls: ['./sign-in.component.scss'],
 })
 export class SignInComponent {
+  animationState: boolean = true;
 
+  constructor() {
+    setTimeout(() => {
+      this.animationState = false;
+    }, 1200);
+  }
 }
