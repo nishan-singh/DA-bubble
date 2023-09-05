d42d2f407341a63a966fcb32a3df54e1a222181a
Nishan Singh <singh007nishan@gmail.com>
2023-09-05 13:43:23 +0200

    added 2 components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SignInComponent } from './components/sign-in/sign-in.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { HomepageComponent } from './components/homepage/homepage.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SignInComponent
+    SignInComponent,
+    SidebarComponent,
+    HomepageComponent
   ],
   imports: [
     BrowserModule,
