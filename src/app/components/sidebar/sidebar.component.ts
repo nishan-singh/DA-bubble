b715c24e196163e97eccc3e25fe551a1f2070e9b
Nishan Singh <singh007nishan@gmail.com>
2023-09-10 16:04:33 +0200

    added basic styling to main chat component

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -3,8 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
-  styleUrls: ['./sidebar.component.scss']
+  styleUrls: ['./sidebar.component.scss'],
 })
 export class SidebarComponent {
-
+  closeSidebar() {
+    document.getElementById('sidebar')?.classList.toggle('sidebar-toggle');
+  }
 }
