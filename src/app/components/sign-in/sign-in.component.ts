cdc6721ce449af0deee1cc9fa4b4ed0166bba501
Nishan Singh <singh007nishan@gmail.com>
2023-09-22 10:24:20 +0200

    added firebase authentication for sign in

--- src/app/components/sign-in/sign-in.component.ts
+++ src/app/components/sign-in/sign-in.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { FormBuilder, Validators } from '@angular/forms';
+import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
 
 @Component({
   selector: 'app-sign-in',
@@ -7,10 +10,35 @@ import { Component } from '@angular/core';
 })
 export class SignInComponent {
   animationState: boolean = true;
+  registerForm = this.fm.group({
+    email: ['', Validators.required],
+    password: ['', Validators.required],
+  });
+  isSubmitted = false;
 
-  constructor() {
+  constructor(private router: Router, private fm: FormBuilder) {
     setTimeout(() => {
       this.animationState = false;
     }, 750);
+  }
+
+  signIn() {
+    const auth = getAuth();
+    signInWithEmailAndPassword(
+      auth,
+      this.registerForm.value.email || '',
+      this.registerForm.value.password || ''
+    )
+      .then((userCredential) => {
+        // Signed in
+        this.router.navigate(['/']);
+        console.log(userCredential.user);
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.log(errorCode, errorMessage);
+      });
+    this.isSubmitted = true;
   }
 }
