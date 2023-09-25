4b61d0b0fb3c595fea21d3fb6b0f001d8ee7c7bf
Nishan Singh <singh007nishan@gmail.com>
2023-09-25 13:50:36 +0200

    connected firebase database with frontend

--- src/app/components/main-chat/main-chat.component.ts
+++ src/app/components/main-chat/main-chat.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import {
+  Firestore,
+  collection,
+  collectionData,
+  doc,
+  getDoc,
+  onSnapshot,
+} from '@angular/fire/firestore';
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
 
 @Component({
   selector: 'app-main-chat',
   templateUrl: './main-chat.component.html',
-  styleUrls: ['./main-chat.component.scss']
+  styleUrls: ['./main-chat.component.scss'],
 })
 export class MainChatComponent {
+  constructor(private firestore: Firestore) {}
 
+  ngOnInit() {
+    const auth = getAuth();
+    onAuthStateChanged(auth, async (user: any) => {
+      if (user) {
+        console.log(user.uid);
+        const userDoc = doc(this.firestore, 'channelsList', user.uid);
+        const userSnapshot = await getDoc(userDoc);
+        console.log(userSnapshot.data());
+      }
+    });
+  }
 }
