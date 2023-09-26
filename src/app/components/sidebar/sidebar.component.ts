fca77b08e285c83cfd949ba3c08f6c73e044d541
Nishan Singh <singh007nishan@gmail.com>
2023-09-26 20:11:16 +0200

    get data from firebase with uid

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Firestore, doc, getDoc } from '@angular/fire/firestore';
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
 
 @Component({
   selector: 'app-sidebar',
@@ -8,8 +10,25 @@ import { Component } from '@angular/core';
 export class SidebarComponent {
   toggleChannels = false;
   toggleDirectMessages = false;
+  channelsList: any;
+  channels: any[] = [];
+
+  constructor(private firestore: Firestore) {}
 
   closeSidebar() {
     document.getElementById('sidebar')?.classList.toggle('sidebar-toggle');
+  }
+
+  ngOnInit() {
+    const auth = getAuth();
+    onAuthStateChanged(auth, async (user: any) => {
+      if (user) {
+        const userDoc = doc(this.firestore, 'channelsList', user.uid);
+        const userSnapshot = await getDoc(userDoc);
+        if (userSnapshot.exists()) {
+          this.channelsList = userSnapshot.data();
+        }
+      }
+    });
   }
 }
