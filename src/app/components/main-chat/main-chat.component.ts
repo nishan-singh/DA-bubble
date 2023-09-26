07e9f5007e605f913a6376b5f5846ea0b9451846
Nishan Singh <singh007nishan@gmail.com>
2023-09-26 20:11:34 +0200

    removed unnecessary functions

--- src/app/components/main-chat/main-chat.component.ts
+++ src/app/components/main-chat/main-chat.component.ts
@@ -1,23 +1,8 @@
 import { Component } from '@angular/core';
-import { Firestore, doc, getDoc } from '@angular/fire/firestore';
-import { getAuth, onAuthStateChanged } from 'firebase/auth';
 
 @Component({
   selector: 'app-main-chat',
   templateUrl: './main-chat.component.html',
   styleUrls: ['./main-chat.component.scss'],
 })
-export class MainChatComponent {
-  constructor(private firestore: Firestore) {}
-
-  ngOnInit() {
-    const auth = getAuth();
-    onAuthStateChanged(auth, async (user: any) => {
-      if (user) {
-        const userDoc = doc(this.firestore, 'channelsList', user.uid);
-        const userSnapshot = await getDoc(userDoc);
-        console.log(userSnapshot.data());
-      }
-    });
-  }
-}
+export class MainChatComponent {}
