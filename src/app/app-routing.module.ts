618250444b9d818bd1b2528c218dc25b965cf2a5
Nishan Singh <singh007nishan@gmail.com>
2023-10-03 12:37:46 +0200

    added channel routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { SignInComponent } from './components/sign-in/sign-in.component';
 import { HomepageComponent } from './components/homepage/homepage.component';
+import { MainChatComponent } from './components/main-chat/main-chat.component';
 
 const routes: Routes = [
   {
@@ -11,6 +12,12 @@ const routes: Routes = [
   {
     path: '',
     component: HomepageComponent,
+    children: [
+      {
+        path: 'channel/:uid',
+        component: MainChatComponent,
+      },
+    ],
   },
 ];
 
