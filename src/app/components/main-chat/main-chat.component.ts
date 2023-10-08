0dc76f6d10715771f3fb51c10f7d69fd7b329422
Nishan Singh <singh007nishan@gmail.com>
2023-10-08 18:08:37 +0200

    added router outlet for chat component

--- src/app/components/main-chat/main-chat.component.ts
+++ src/app/components/main-chat/main-chat.component.ts
@@ -1,8 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Firestore } from '@angular/fire/firestore';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-main-chat',
   templateUrl: './main-chat.component.html',
   styleUrls: ['./main-chat.component.scss'],
 })
-export class MainChatComponent {}
+export class MainChatComponent {
+  @Input() id: string = '';
+
+  constructor(
+    private firestore: Firestore,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {
+    // console.log(this.id);
+    this.route.paramMap.subscribe((params) => {
+      this.id = params.get('id') || '';
+      console.log(this.id);
+      this.router.navigate([this.id]);
+    });
+    // console.log(this.route.snapshot.paramMap.get('id'));
+  }
+}
